0f6fd1329e223c30599e5ed9f7557f65d3946243
Phan Khanh Thien <thien061097@gmail.com>
2018-06-13 22:02:31 +0700

    First init

---
+++ App.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  asdasddsdasdsadasds
+</div>
+</template>
+
+<script>
+export default {
+  methods: {
+    print () {
+      console.log('Thien');
+    }
+  },
+  mounted () {
+    console.log('created');
+    this.print()
+  }
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CircularDependencyPlugin = require('circular-dependency-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+  mode: process.env.WEBPACK_SERVE ? 'development' : 'production',
+  entry: './src/index.js',
+  output: {
+    path: '/dist',
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.vue$/,
+        exclude: /node_modules/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  plugins: [
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // add errors to webpack instead of warnings
+      failOnError: true,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd(),
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html' //root of the application
+  }),
+    new VueLoaderPlugin({
+      hotReload: true
+    })
+  ],
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+    }
+  }
+};
